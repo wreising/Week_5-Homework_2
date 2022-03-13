f9de9a98bbda0435fe2a5d3f32eeb0f24f41c683
wreising <wreising@mac.com>
2022-03-13 00:54:32 -0800

    readme and flowchart updates

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const Manager = require("./lib/Manager")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 const inquirer = require("inquirer")
-const path = require("path")
+const path = require("path") // ?
 const fs = require("fs")
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html")
+const OUTPUT_DIR = path.resolve(__dirname, "output") // ?
+const outputPath = path.join(OUTPUT_DIR, "team.html") // ?
 
-const render = require("./lib/htmlRenderer")
+const render = require("./lib/htmlRenderer") // how do I send stuff here?
 
 // open array
 let teamArray = []
@@ -155,6 +155,7 @@ function addIntern() {
 
 // send the array to the htmlRenderer.js
 function createTeamPage() {
+  // should show the complete team array
   console.log(teamArray)
   // send to the rencerer how? - need to watch more of the video
 }
