0f1985a21a1a3eca4060d9e2843def9130939029
wreising <wreising@mac.com>
2022-03-13 12:28:36 -0700

    deleted main

--- __Main - DELETE/reset.js
+++
@@ -1,8 +0,0 @@
-// Removes the team.html file from the dist/ folder.
-// Used in `npm run reset` script
-
-const fs = require('fs');
-if (fs.existsSync('./dist/team.html')) {
-  fs.unlinkSync('./dist/team.html');
-  console.log('/dist/ folder reset!');
-}
